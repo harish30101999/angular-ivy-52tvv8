bc42f6df09ffa9bbc724c5cfd877a6e249b2e3dc
harish30101999 <109505934+harish30101999@users.noreply.github.com>
2022-11-16 10:18:27 +0530

    2 files modified

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # angular-ivy-52tvv8
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-52tvv8)
--- src/app/ticketbooking/ticketbooking.service.ts
+++ src/app/ticketbooking/ticketbooking.service.ts
@@ -1,41 +1,41 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Ticketbooking } from './ticketbooking';
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Ticketbooking } from './ticketbooking';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class TicketbookingService {
-  constructor(private httpClient: HttpClient) {}
-  public getAllTicketbookingService() {
-    return this.httpClient.get('http://localhost:8080/ticketbooking/');
-  }
-  public getTicketbookingService(id) {
-    return this.httpClient.get('http://localhost:8080/ticketbooking/' + id);
-  }
-  public deleteTicketbookingService(id) {
-    return this.httpClient.delete('http://localhost:8080/ticketbooking/' + id);
-  }
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class TicketbookingService {
+//   constructor(private httpClient: HttpClient) {}
+//   public getAllTicketbookingService() {
+//     return this.httpClient.get('http://localhost:8080/ticketbooking/');
+//   }
+//   public getTicketbookingService(id) {
+//     return this.httpClient.get('http://localhost:8080/ticketbooking/' + id);
+//   }
+//   public deleteTicketbookingService(id) {
+//     return this.httpClient.delete('http://localhost:8080/ticketbooking/' + id);
+//   }
   
-  public createTicketbookingService(ticketbooking: Ticketbooking) {
-   // alert(JSON.stringify(ticketbooking));
-    const headers = { 'content-type': 'application/json' };
-    return this.httpClient.post(
-      'http://localhost:8080/ticketbooking/',
-      JSON.stringify(ticketbooking),
-      {
-        headers: headers,
-      }
-    );
-  }
-  public updateTicketbookingService(id, ticketbooking: Ticketbooking) {
-    const headers = { 'content-type': 'application/json' };
-    return this.httpClient.put(
-      'http://localhost:8080/ticketbooking/' + id,
-      JSON.stringify(ticketbooking),
-      {
-        headers: headers,
-      }
-    );
-  }
-}
+//   public createTicketbookingService(ticketbooking: Ticketbooking) {
+//    // alert(JSON.stringify(ticketbooking));
+//     const headers = { 'content-type': 'application/json' };
+//     return this.httpClient.post(
+//       'http://localhost:8080/ticketbooking/',
+//       JSON.stringify(ticketbooking),
+//       {
+//         headers: headers,
+//       }
+//     );
+//   }
+//   public updateTicketbookingService(id, ticketbooking: Ticketbooking) {
+//     const headers = { 'content-type': 'application/json' };
+//     return this.httpClient.put(
+//       'http://localhost:8080/ticketbooking/' + id,
+//       JSON.stringify(ticketbooking),
+//       {
+//         headers: headers,
+//       }
+//     );
+//   }
+// }
