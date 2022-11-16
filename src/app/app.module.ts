59e20b9a705c089af179f1d6a2a5bed9498036e7
harish30101999 <109505934+harish30101999@users.noreply.github.com>
2022-11-16 12:18:15 +0530

    2 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
 
 import { TicketbookingComponent } from './ticketbooking/ticketbooking.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, HelloComponent, TicketbookingComponent],
+  imports: [BrowserModule,ReactiveFormsModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, TicketbookingComponent],
   bootstrap: [AppComponent, TicketbookingComponent],
 })
 export class AppModule {}
+
+// import { Component, Input } from '@angular/core';
+
+// @Component({
+//   selector: 'hello',
+//   template: `<h1>Hello {{name}}!</h1>`,
+//   styles: [`h1 { font-family: Lato; }`]
+// })
+// export class HelloComponent  {
+//   @Input() name: string;
+// }
+
