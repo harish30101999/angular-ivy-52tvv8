da78dbc8c25b58716d0173a28976d26bae9da7e3
harish30101999 <109505934+harish30101999@users.noreply.github.com>
2022-11-16 12:28:27 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'app-ticketbooking',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
